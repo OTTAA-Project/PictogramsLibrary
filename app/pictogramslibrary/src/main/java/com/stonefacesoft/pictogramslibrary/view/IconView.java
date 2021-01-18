8e8c215630c6c3d5bea85e66ee2c3698a421c0bb
gonojuarez <gonojuarez17@gmail.com>
2021-01-18 17:16:56 -0300

    v 0.1.1 license edited

---
+++ app/pictogramslibrary/src/main/java/com/stonefacesoft/pictogramslibrary/view/IconView.java
@@ -0,0 +1,25 @@
+package com.stonefacesoft.pictogramslibrary.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class IconView extends TarjetView{
+    public IconView(@NonNull Context context) {
+        super(context);
+    }
+
+    public IconView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IconView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public IconView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
