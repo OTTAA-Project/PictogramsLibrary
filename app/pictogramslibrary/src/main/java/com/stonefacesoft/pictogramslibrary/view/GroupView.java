4ff338e442d7ec8fe1f99b9b61b8117e7c6602d6
gonojuarez <gonojuarez17@gmail.com>
2021-08-10 12:22:35 -0300

    version 0.1.8.19 prerelease

--- app/pictogramslibrary/src/main/java/com/stonefacesoft/pictogramslibrary/view/GroupView.java
+++ app/pictogramslibrary/src/main/java/com/stonefacesoft/pictogramslibrary/view/GroupView.java
@@ -7,19 +7,17 @@ import android.net.Uri;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import com.bumptech.glide.Glide;
 import com.stonefacesoft.pictogramslibrary.Classes.Group;
 import com.stonefacesoft.pictogramslibrary.R;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import org.json.JSONArray;
-
 import java.io.File;
 
-public class GroupView extends TarjetView{
-    private ImageView tagHora,tagUbicacion,tagSexo,tagEdad;
+public class GroupView extends TarjetView {
+    private ImageView tagHora, tagUbicacion, tagSexo, tagEdad;
     private ImageView Color;
     private int Custom_Color;
     private Group pictogramsLibraryGroup;
@@ -69,10 +67,10 @@ public class GroupView extends TarjetView{
         this.icon = findViewById(R.id.grid_image);
         this.Color = findViewById(R.id.color_Picto);
         this.kind_of_pictogram = findViewById(R.id.kind_of_Picto);
-        this.tagEdad=findViewById(R.id.tagCalendario);
-        this.tagHora=findViewById(R.id.tagHora);
-        this.tagUbicacion=findViewById(R.id.tagUbicacion);
-        this.tagSexo=findViewById(R.id.tagClima);
+        this.tagEdad = findViewById(R.id.tagCalendario);
+        this.tagHora = findViewById(R.id.tagHora);
+        this.tagUbicacion = findViewById(R.id.tagUbicacion);
+        this.tagSexo = findViewById(R.id.tagClima);
     }
 
     public void setCustom_Img(Drawable imagen) {
@@ -82,8 +80,8 @@ public class GroupView extends TarjetView{
         requestLayout();
     }
 
-    public void setName(String name){
-                StrTittle.setText(name);
+    public void setName(String name) {
+        StrTittle.setText(name);
     }
 
     public void setPictogramsLibraryGroup(Group pictogramsLibraryGroup) {
@@ -91,37 +89,36 @@ public class GroupView extends TarjetView{
         setData();
     }
 
-    private void setData(){
-        id= pictogramsLibraryGroup.getId();
+    private void setData() {
+        id = pictogramsLibraryGroup.getId();
         this.setCustom_Texto(pictogramsLibraryGroup.getObjectName());
-        if(pictogramsLibraryGroup.getEditedPictogram().isEmpty()){
+        if (pictogramsLibraryGroup.getEditedPictogram().isEmpty()) {
             glideAttatcher.useDiskCacheStrategy().loadDrawable(mContext.getResources().getDrawable(mContext.getResources().getIdentifier(pictogramsLibraryGroup.getPictogram(),
-                    "drawable", mContext.getPackageName())),this.icon);
-        }else{
-            File picto=new File(pictogramsLibraryGroup.getEditedPictogram());
-            if(picto.exists())
-                glideAttatcher.useDiskCacheStrategy().loadDrawable(picto,this.icon);
+                    "drawable", mContext.getPackageName())), this.icon);
+        } else {
+            File picto = new File(pictogramsLibraryGroup.getEditedPictogram());
+            if (picto.exists())
+                glideAttatcher.useDiskCacheStrategy().loadDrawable(picto, this.icon);
             else
-                glideAttatcher.useDiskCacheStrategy().loadDrawable(Uri.parse(pictogramsLibraryGroup.getUrl()),this.icon);
+                glideAttatcher.useDiskCacheStrategy().loadDrawable(Uri.parse(pictogramsLibraryGroup.getUrl()), this.icon);
         }
-  }
+    }
 
-  public void loadHourIcon(boolean value){
-      tagHora.setImageResource(value?R.drawable.ic_timer_black_24dp:R.drawable.ic_timer_off_black_24dp);
-  }
-  public void loadGenderIcon(boolean value){
-      tagSexo.setImageResource(value?R.drawable.ic_baseline_wc_24:R.drawable.ic_baseline_disable_24);
+    public void loadHourIcon(boolean value) {
+        tagHora.setImageResource(value ? R.drawable.ic_timer_black_24dp : R.drawable.ic_timer_off_black_24dp);
+    }
 
-  }
-  public void loadLocationIcon(boolean value){
-      tagUbicacion.setImageResource(value?R.drawable.ic_location_on_black_24dp:R.drawable.ic_location_off_black_24dp);
-  }
+    public void loadGenderIcon(boolean value) {
+        tagSexo.setImageResource(value ? R.drawable.ic_baseline_wc_24 : R.drawable.ic_baseline_disable_24);
+    }
 
-  public void loadAgeIcon(boolean value){
-      tagUbicacion.setImageResource(value?R.drawable.ic_face_black_on_24dp:R.drawable.ic_face_black_24dp);
+    public void loadLocationIcon(boolean value) {
+        tagUbicacion.setImageResource(value ? R.drawable.ic_location_on_black_24dp : R.drawable.ic_location_off_black_24dp);
+    }
 
-  }
-
+    public void loadAgeIcon(boolean value) {
+        tagEdad.setImageResource(value ? R.drawable.ic_face_black_on_24dp : R.drawable.ic_face_black_24dp);
+    }
 
 
     public void setCustom_Texto(String t) {
